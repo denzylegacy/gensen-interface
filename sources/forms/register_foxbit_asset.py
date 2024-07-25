97e490803a0694d98953fee90658a5631a01d3de
Denzy <alissondin.developer@gmail.com>
2024-07-25 19:40:50 -0300

    25.07.2024.1

--- sources/forms/register_foxbit_asset.py
+++ sources/forms/register_foxbit_asset.py
@@ -48,15 +48,15 @@ class FoxbitAssetRegistration(Modal, title="Asset registration"):
                 color=0xffa07a
             )
             embed.add_field(
-                name="", 
+                name="",
                 value="Please check whether the information you entered is correct. If you believe this is a bug, please contact my developer!",
                 inline=False
             )
             await interaction.followup.send(embed=embed, ephemeral=True)
             return
-        
+
         currency_name = self.asset.value.strip().lower()
-        
+
         firebase = Firebase()
 
         connection = firebase.firebase_connection("root")
@@ -64,32 +64,42 @@ class FoxbitAssetRegistration(Modal, title="Asset registration"):
         user_credentials = connection.child(
             f"users/{interaction.user.id}/exchanges/foxbit/credentials"
         ).get()
-        
+
         foxbit = Foxbit(
             api_key=Encryptor().decrypt_api_key(user_credentials["FOXBIT_ACCESS_KEY"]),
             api_secret=Encryptor().decrypt_api_key(user_credentials["FOXBIT_SECRET_KEY"])
         )
 
         currency = foxbit.check_currency(currency=currency_name)
-        
+
         if not currency:
             await interaction.followup.send(
                 "The cryptocurrency you are looking for is not registered with Foxbit! Please try again later or contact my developers.",
                 ephemeral=True
             )
             return
-        
+
         user_assets = connection.child(
             f"users/{interaction.user.id}/exchanges/foxbit/cryptocurrencies"
         ).get()
 
-        asset_names = [user_assets[y]["name"].lower() for y in user_assets] + list(user_assets.keys())
-        
+        if not currency:
+            await interaction.followup.send(
+                "The cryptocurrency you are looking for is not registered with Foxbit! Please try again later or contact my developers.",
+                ephemeral=True
+            )
+            return
+
+        asset_names = None
+
+        if user_assets:
+            asset_names = [user_assets[y]["name"].lower() for y in user_assets] + list(user_assets.keys())
+
         if not user_assets or currency_name not in asset_names:
             timestamp = datetime.datetime.now(
                 pytz.timezone("America/Sao_Paulo")
             ).strftime("%Y-%m-%d %H:%M:%S")
-            
+
             connection.child(
                 f"users/{interaction.user.id}/exchanges/foxbit/cryptocurrencies/{currency['symbol'].lower()}"
             ).update(
@@ -101,16 +111,16 @@ class FoxbitAssetRegistration(Modal, title="Asset registration"):
                 }
             )
             log.info(f"[LINKED] {currency_name.upper()} -> {interaction.user.name} ({interaction.user.id})")
-            
+
             resp = (
                 f"Asset: {currency_name.upper()}\n"
                 f"Base Balance (BRL): {self.base_balance.value}\n"
                 f"Fixed Profit (BRL): {self.fixed_profit_brl.value}"
             )
-            
+
             embed = discord.Embed(title="Successfully registered!", description=resp, color=0x6AA84F)
             embed.add_field(name="", value="Your asset has been registered successfully!", inline=False)
-            
+
             await interaction.followup.send(embed=embed, ephemeral=True)
         else:
             embed = discord.Embed(
@@ -118,9 +128,9 @@ class FoxbitAssetRegistration(Modal, title="Asset registration"):
                 description=f"The **{self.asset.value}** asset **is** already linked to your account!!",
                 color=0xffa07a
             )
-            
+
             embed.add_field(
-                name="", 
+                name="",
                 value="Please check whether the information you entered is correct. If you believe this is a bug, please contact my developer!",
                 inline=False
             )
