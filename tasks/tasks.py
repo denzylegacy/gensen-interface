a66b4926c7c8e940a0455bc80eb3b2ab0114050d
Denzy <108025321+denzylegacy@users.noreply.github.com>
2024-09-22 19:20:00 -0300

    Update tasks.py

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -58,7 +58,7 @@ class BackgroundTasks(commands.Cog):
     
                     connection.child(f"users/{user}/messages/{messenger}/{message}").delete()
 
-    @tasks.loop(seconds=180)
+    @tasks.loop(seconds=60)
     async def background_tasks(self):
         await self.market_conditions_evaluator()
 
