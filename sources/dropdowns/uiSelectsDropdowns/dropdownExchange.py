46ffea0e4951b2c594f4abc3947e8d1f94dec575
Denzy <alissondin.developer@gmail.com>
2025-01-08 20:38:03 -0300

    dropdown bug fix

--- sources/dropdowns/uiSelectsDropdowns/dropdownExchange.py
+++ sources/dropdowns/uiSelectsDropdowns/dropdownExchange.py
@@ -8,7 +8,6 @@ from utils.utilities import UniqueIdGenerator
 from sources.forms.register_bitcoin_address import BitcoinAddressRegistration
 
 data_options = JSONHandler(file_path="./infra/options.json").read_json()
-# from sources.dropdowns.dropdown import DropdownView
 
 
 class DropdownExchange(discord.ui.Select):
@@ -39,7 +38,7 @@ class DropdownExchange(discord.ui.Select):
 
     async def callback(self, interaction: discord.Interaction):
         try:
-            # await interaction.response.defer(ephemeral=True)
+            await interaction.response.defer(ephemeral=True)
 
             assert self.view is not None
             
@@ -48,46 +47,51 @@ class DropdownExchange(discord.ui.Select):
             view.value = self.labels.index(self.values[0])
             view.interaction = interaction
 
+            print(f"chones option: {view.value}")
+            
             if int(view.value) == 0:  # ADD/UPDATE BITCOIN ADDRESS
                 try:
                     await interaction.response.send_modal(BitcoinAddressRegistration())
                 except:
                     print(traceback.format_exc())
             elif int(view.value) == 1:
-                from sources.dropdowns.dropdown import DropdownView
+                try:
+                    from sources.dropdowns.dropdown import DropdownView
 
-                embed = (
-                    CustomEmbed(
-                        "Foxbit Settings",
-                        "Connect Gensen to your Foxbit account by adding the API keys which can be obtained from the [official website](https://app.foxbit.com.br/profile/api-key);\n\n"
-                        "Link a new crypto asset to your portfolio;\n\n"
-                        "Completely remove the data from your exchange account in Gensen's database by clicking disconnect."
-                    ) #  https://cdn.pfps.gg/banners/1785-chainsaw-man-cinema.gif
-                	.set_image("https://cdn.pfps.gg/banners/9031-toney.gif")
-                	.create_embed()
-                )
-
-                unique_id = UniqueIdGenerator.generate_unique_custom_id()
-                options = data_options["dropdown_exchange_settings"]["dropdown"]
-                
-                view = DropdownView(
-                    options, 
-                    dropdown_name="DropdownExchangeSettings",
-                    placeholder=data_options["dropdown_exchange_settings"]["dropdown_placeholder"], 
-                    custom_id_dropdown=f"dropdown_{unique_id}",
-                    custom_id_button=unique_id
-                )
-                
-                embed = (
-                    CustomEmbed(
-                        "Foxbit Settings",
-                        "Connect Gensen to your Foxbit account by adding the API keys which can be obtained from the [official website](https://app.foxbit.com.br/profile/api-key)."
+                    embed = (
+                        CustomEmbed(
+                            "Foxbit Settings",
+                            "Connect Gensen to your Foxbit account by adding the API keys which can be obtained from the [official website](https://app.foxbit.com.br/profile/api-key);\n\n"
+                            "Link a new crypto asset to your portfolio;\n\n"
+                            "Completely remove the data from your exchange account in Gensen's database by clicking disconnect."
+                        ) #  https://cdn.pfps.gg/banners/1785-chainsaw-man-cinema.gif
+                        .set_image("https://cdn.pfps.gg/banners/9031-toney.gif")
+                        .create_embed()
                     )
-                    .set_image("https://cdn.pfps.gg/banners/9031-toney.gif")
-                    .create_embed()
-                )
-                
-                await interaction.followup.send(embed=embed, view=view, ephemeral=True)
+
+                    unique_id = UniqueIdGenerator.generate_unique_custom_id()
+                    options = data_options["dropdown_exchange_settings"]["dropdown"]
+                    
+                    view = DropdownView(
+                        options, 
+                        dropdown_name="DropdownExchangeSettings",
+                        placeholder=data_options["dropdown_exchange_settings"]["dropdown_placeholder"], 
+                        custom_id_dropdown=f"dropdown_{unique_id}",
+                        custom_id_button=unique_id
+                    )
+                    
+                    embed = (
+                        CustomEmbed(
+                            "Foxbit Settings",
+                            "Connect Gensen to your Foxbit account by adding the API keys which can be obtained from the [official website](https://app.foxbit.com.br/profile/api-key)."
+                        )
+                        .set_image("https://cdn.pfps.gg/banners/9031-toney.gif")
+                        .create_embed()
+                    )
+                    
+                    await interaction.followup.send(embed=embed, view=view, ephemeral=True)
+                except:
+                    print(traceback.format_exc())
         except Exception as e:
             print(f"An error occurred: {e}")
             tb = traceback.format_exc()
